d8a3ca0af6cf361c5413008a27b46d5c1f7f599d
milossimic <milossimicsimo@gmail.com>
2019-10-26 23:33:09 +0200

    added simple logic just to test push for now

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
-	gPb "github.com/c12s/scheme/gravity"
+	"fmt"
+	fPb "github.com/c12s/scheme/flusher"
 	"github.com/golang/protobuf/proto"
 	"github.com/nats-io/go-nats"
+	"runtime"
 )
 
+const subject = "topology.regions.>"
+
 func main() {
+	natsConnection, _ := nats.Connect("0.0.0.0:4222")
+	natsConnection.Subscribe(subject, func(msg *nats.Msg) {
+		data := fPb.FlushPush{}
+		err := proto.Unmarshal(msg.Data, &data)
+		if err == nil {
+			// Handle the message
+			fmt.Println(data)
+		}
+	})
+
+	// Keep the connection alive
+	runtime.Goexit()
+
 }
